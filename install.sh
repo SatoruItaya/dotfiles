d3f46cd269efab0a4456e7a008e561caa709235d
SatoruItaya <str.itaya@gmail.com>
2022-04-29 22:55:53 +0900

    fix gitignore copy command

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ IGNORE_PATTERN="^\.(git)$"
 for dotfile in .??*; do
     [[ $dotfile =~ $IGNORE_PATTERN ]] && continue
         case "$dotfile" in
-            ".gitignore" ) cp "$(pwd)/$dotfile" "$HOME/.config/git/ignore" ;;
+            ".gitignore" ) mkdir "$HOME/.config/git" ; cp "$(pwd)/$dotfile" "$HOME/.config/git/ignore" ;;
             * ) ln -snfv "$(pwd)/$dotfile" "$HOME/$dotfile" ;;
         esac
 done
